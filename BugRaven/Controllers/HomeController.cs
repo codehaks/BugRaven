e7e94d8aa97845bef9f734d17f7bafb2d8807fed
Hakim Ghods <ghods.hakim@hotmail.com>
2018-04-10 00:08:33 +0430

    Add project files.

---
+++ BugRaven/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BugRaven.Models;
+
+namespace BugRaven.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Models.Bug> model = new List<Models.Bug>();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bug model)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BugRaven/Models/Bug.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugRaven.Models
+{
+    public class Bug
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
